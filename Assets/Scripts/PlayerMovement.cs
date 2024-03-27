6e0cee482b2f800b205dd8a26021afd61f60aab1
Harris <h.s.kim@wustl.edu>
2024-03-27 13:38:37 -0500

    new movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,23 +6,52 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
 
+    public float moveSpeedLeft = 40f;
+    public float moveSpeedRight = 40f;
+    public float accelerationRate = 70f;
+    public float maxSpeed = 150f;
+    public float defaultSpeed = 40f;
+
+    private bool isFacingLeft;
+
     // Start is called before the first frame update
     void Start()
     {
+        isFacingLeft = true;
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //gets the mouse position
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = transform.position.z;
+        //move left
+        if(Input.GetKey(KeyCode.A))
+        {
+            moveSpeedLeft = Mathf.Min(moveSpeedLeft + accelerationRate * Time.deltaTime, maxSpeed);
+            rb.AddForce(Vector2.left * moveSpeedLeft * Time.deltaTime, ForceMode2D.Impulse);
+        }
+        else if(Input.GetKeyUp(KeyCode.A))
+        {
+            moveSpeedLeft = defaultSpeed;
+        }
+
+        //move right
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveSpeedRight = Mathf.Min(moveSpeedRight + accelerationRate * Time.deltaTime, maxSpeed);
+            rb.AddForce(Vector2.right * moveSpeedRight * Time.deltaTime, ForceMode2D.Impulse);
+        }
+        else if(Input.GetKeyUp(KeyCode.D))
+        {
+            moveSpeedRight = defaultSpeed;
+        }
 
 
-        Vector3 newPosition = new Vector3(mousePosition.x, -2.5f, mousePosition.z);
-
-        rb.MovePosition(newPosition);
-
+        //do the face switching
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            isFacingLeft = !isFacingLeft;
+            //do animations
+        }
     }
 }
