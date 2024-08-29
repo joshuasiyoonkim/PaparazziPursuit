d733639639c7dc5e2fbe783a27eb87657c63a3c8
Harris <h.s.kim@wustl.edu>
2024-08-29 13:34:44 -0500

    animation loop and timing

--- Assets/Scripts/SpriteVisibilityController.cs
+++ Assets/Scripts/SpriteVisibilityController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class SpriteVisibilityController : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
+    public Animator animator;
+    public string animationName;
+    private int loopCount = 0;
+    public int maxLoops = 3; // Set to 2 for looping twice
 
     void Start()
     {
@@ -14,11 +18,29 @@ public class SpriteVisibilityController : MonoBehaviour
 
     public void ShowSprite()
     {
-        spriteRenderer.enabled = true; // Show the sprite when called by the animation event
+        spriteRenderer.enabled = true; // Show the sprite at the start of the animation
+    }
+
+    public void CheckAndLoopOrHide()
+    {
+        Debug.Log("check loop function called");
+        loopCount++;
+        if (loopCount < maxLoops)
+        {
+            // Restart the animation to loop again
+            animator.Play(animationName, 0, 0);
+        }
+        else
+        {
+            // After the final loop, hide the sprite
+            HideSprite();
+            animator.SetTrigger("stopAnim");
+            loopCount = 0;
+        }
     }
 
     public void HideSprite()
     {
-        spriteRenderer.enabled = false; // Hide the sprite when called by the animation event
+        spriteRenderer.enabled = false; // Hide the sprite after the final loop
     }
 }
